8017d2a0c565e0e4dbb766188cbf28844baa727b
gr33ngr33n <actfun07@gmail.com>
2020-08-28 16:36:12 +0800

    use ListView.builder to create scrollable transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,47 +10,50 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height:
-          485, // you need to fix the height so that the scroll knows how hieght it can take!!!!!!
-      child: ListView(
-        // ListView is similar to shortcut of SingleChildScrollView + Column. ListView has infinite height. Therefore, we need the height to be fixed (IMPORTANT!!!!!)
-        children: transactions.map((tx) {
-          return Card(
-            child: Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
-                  child: Text(
-                    '\$ ${tx.amount}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tx.title,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        height:
+            485, // you need to fix the height so that the scroll knows how hieght it can take!!!!!!
+
+        // better way of implementing ListView is using ListView.builder. ListView.builder only renders items that are visible (good for performance)
+        // normal ListView on another hand, renders ALL items regardless of visible or not (bad for performance when dealing with long list items)
+        child: ListView.builder(
+          itemBuilder: (buildCtx, idx) {
+            // idx refers to index of the list items
+            return Card(
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    child: Text(
+                      '\$ ${transactions[idx].amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
                     ),
-                    Text(
-                      new DateFormat().format(tx.date),
-                      // new DateFormat('yyyy-mm-dd').format(tx.date),  // you can also do this
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
-                )
-              ],
-            ),
-          );
-        }).toList(),
-      ),
-    );
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transactions[idx].title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text(
+                        new DateFormat().format(transactions[idx].date),
+                        // new DateFormat('yyyy-mm-dd').format(tx.date),  // you can also do this
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+          itemCount: transactions.length,
+        ));
   }
 }
