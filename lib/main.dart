6be41f3d376e06f636adb7ca0389b6fff8b21d1d
gr33ngr33n <actfun07@gmail.com>
2020-08-31 12:41:31 +0800

    added FloatingActionButton & IconButton

--- lib/main.dart
+++ lib/main.dart
@@ -17,26 +17,34 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                margin: EdgeInsets.all(5),
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Chart'),
-                  elevation: 5,
-                ),
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        actions: [IconButton(icon: Icon(Icons.add), onPressed: null)],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.all(5),
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Chart'),
+                elevation: 5,
               ),
-              UserTransactions()
-            ],
-          ),
-        ));
+            ),
+            UserTransactions()
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation
+          .centerFloat, // to adjust floatingActionButton alignment/location
+    );
   }
 }
