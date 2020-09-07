eaa9bc18b4b0837e2151e070086a85ae225203b1
gr33ngr33n <actfun07@gmail.com>
2020-09-07 16:05:47 +0800

    deducted height of statusbar (calculate height dynamically)

--- lib/main.dart
+++ lib/main.dart
@@ -110,13 +110,15 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             Container(
               height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height) *
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
                   0.4,
               child: Chart(_recentTransactions),
             ),
             Container(
               height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height) *
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
                   0.6,
               child: TransactionList(_userTransactions, _deleteTransaction),
             )
