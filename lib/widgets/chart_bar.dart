ddaf68aeb9a7da9cc8e5fda6372e546f0d318be1
gr33ngr33n <actfun07@gmail.com>
2020-09-07 16:14:19 +0800

    use layout builder widget

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -10,45 +10,52 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height:
-              20, // fix the Fittedbox height to 20px no matter how much the text grow
-          child: FittedBox(
-            // if the content of the text grows, it will not wrap, it will shrink its content instead
-            child: Text('\$${amount.toStringAsFixed(2)}'),
+    return LayoutBuilder(builder: (context, constraints) {
+      return Column(
+        children: [
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              // if the content of the text grows, it will not wrap, it will shrink its content instead
+              child: Text('\$${amount.toStringAsFixed(2)}'),
+            ),
           ),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          // Stack widget allows to stack elements on top of each other (in 3D way)
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey, width: 1.0),
-                    color: Color.fromRGBO(220, 220, 220, 1),
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-              FractionallySizedBox(
-                heightFactor: totalShare,
-                child: Container(
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            // Stack widget allows to stack elements on top of each other (in 3D way)
+            child: Stack(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
+                      border: Border.all(color: Colors.grey, width: 1.0),
+                      color: Color.fromRGBO(220, 220, 220, 1),
                       borderRadius: BorderRadius.circular(10)),
                 ),
-              )
-            ],
+                FractionallySizedBox(
+                  heightFactor: totalShare,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        SizedBox(height: 5),
-        Text(label)
-      ],
-    );
+          SizedBox(height: constraints.maxHeight * 0.05),
+          Container(
+            padding: EdgeInsets.all(5),
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(label),
+            ),
+          )
+        ],
+      );
+    });
   }
 }
